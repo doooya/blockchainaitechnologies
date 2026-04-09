e8b139a4ee02d730fefc7223ee44afd067bc6790
Nada Emara <doooya@me.com>
2026-04-09 14:44:50 +0100

    Fix document.js with AdSense and Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google AdSense */}
         <meta name="google-adsense-account" content="ca-pub-5906025502715825" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5906025502715825" crossOrigin="anonymous" />
-
-        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-WSEVLTNRVR" />
         <script
           dangerouslySetInnerHTML={{
@@ -19,15 +16,11 @@ export default function Document() {
             `,
           }}
         />
-        {/* Organization schema */}
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': 'https://schema.org',
-              '@type': 'Organization',
-              name: 'BlockchainAI Technologies',
-              url: 'https://blockchainaitechnologies.com',
-              logo: 'https://blockchainaitechnologies.com/images/logo.png',
-              sameAs: [
-                'https://twitter.com/BlockchainAITech'
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
